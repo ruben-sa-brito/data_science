3616b9b1282c7181ae57189cc53f5c367ec4e18c
Timewound <rubenjms1@gmail.com>
2023-01-11 21:52:20 -0300

    init

--- titan.py
+++ titan.py
@@ -5,5 +5,8 @@ tit = pd.read_csv('titanic.csv') # leitura do arquivo 'titanic.csv'
 
 tit  = tit[['Name', 'Age', 'Sex','Pclass','Survived']] #seleciona colunas específicas
 
-tit.loc[(tit['Survived']==1) & (tit['Pclass'] == 1) & (tit['Sex'] == 'female')].to_csv('titanic_result.csv') #adiciona filtros, e cria um novo arquivo csv
+tit = tit.loc[(tit['Survived']==1) & (tit['Pclass'] == 1) & (tit['Sex'] == 'female')] #adiciona filtros
 
+tit = tit.sort_values(['Name'], ascending= True) #organiza os dados em ordem alfabética 
+
+tit.to_csv('titanic_result.csv') #cria um arquivo csv com as novas modificações
